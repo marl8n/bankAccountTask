9561041bd9763abedbc6814757d17fb3a4efdc8d
Marl8n <midp515@gmail.com>
2020-09-08 16:36:00 -0600

    Se establecio la UI del programa

---
+++ main/java/com/mycompany/bankaccounts/User/User.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bankaccounts.User;
+
+/**
+ *
+ * @author marl8n
+ */
+public class User {
+    private String dpi;
+    private String name;
+    private Double balance;
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
